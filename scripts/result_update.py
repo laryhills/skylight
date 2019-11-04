5222501d3efe037480d63ef09a6858490df73bb1
chima <chimaataman@gmail.com>
2019-11-04 05:10:39 +0100

    add result update endpoint (using an isolated server)

--- scripts/result_update.py
+++ scripts/result_update.py
@@ -1,12 +1,28 @@
+import csv
+import os
+
 from flask import Flask, render_template, url_for
 from flask_weasyprint import HTML, render_pdf
+import itertools
+
 
 app = Flask(__name__)
+base_dir = os.path.dirname(__file__)
+db_path = os.path.join(base_dir, 'database')
 
 
-@app.route('/update/<mat_no>', defaults={'mat_no': 'ENG1503886'})
-def get_name(mat_no):
-    return render_template('result_update.html', name=mat_no)
+@app.route('/update/', defaults={'mat_no': 'ENG1503886', 'session': '2016'})
+@app.route('/update/<mat_no>/<session>')
+def get_name(mat_no, session):
+    results = []
+    row_number, column_number = 0, 1
+    with open(os.path.join(base_dir,'templates', 'Score_Sheet.csv'), 'r') as scores:
+        row = next(itertools.islice(csv.reader(scores), row_number, row_number + 1))
+        print('1')
+        if (row[column_number+2] == mat_no) and (row[column_number+1] == session):
+            results.append((row[column_number], row[column_number+3]))
+            print(results)
+    return render_template('result_update.html', name=mat_no, results=results[0])
 
 
 @app.route('/hello/', defaults={'name': 'World'})
