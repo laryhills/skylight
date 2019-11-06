e4d344144cc4204de35b9cee75140b43491af1d4
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2019-11-06 23:27:03 +0100

    Refined result_update.py

--- scripts/result_update.py
+++ scripts/result_update.py
@@ -2,8 +2,7 @@ import csv
 import os
 
 from flask import Flask, render_template, url_for
-from flask_weasyprint import HTML, render_pdf
-import itertools
+from flask_weasyprint import render_pdf
 
 
 app = Flask(__name__)
@@ -16,29 +15,18 @@ db_path = os.path.join(base_dir, 'database')
 def get_name(mat_no, session):
     results = []
     row_number, column_number = 0, 0
-    scores = open(os.path.join(base_dir,'templates', 'Score_Sheet.csv'), 'r')
+    scores = open(os.path.join(base_dir, 'templates', 'Score_Sheet.csv'), 'r')
     csv_reader = csv.reader(scores, delimiter=',')
     for row in csv_reader:
         if (row[column_number+2] == mat_no) and (row[column_number+1] == session):
             results.append((row[column_number], row[column_number+3]))
-    return render_template('result_update.html', name=mat_no, results=results)
-
-
-@app.route('/hello/', defaults={'name': 'World'})
-@app.route('/hello/<name>/')
-def hello_html(name):
-    return render_template('result_update.html', name=name)
+    return render_template('result_update.html', name=mat_no, results=results, session=session)
 
 
 @app.route('/update_<mat_no>_<session>.pdf')
-def get_pdf(mat_no, session, results=[]):
+def get_pdf(mat_no, session):
     # Make a PDF from another view
     return render_pdf(url_for('get_name', mat_no=mat_no, session=session))
-
-# def get_pdf(name):
-#     # Make a PDF straight from HTML in a string.
-#     html = render_template('result_update.html', name=name)
-#     return render_pdf(HTML(string=html))
 
 
 if __name__ == '__main__':
