12f257ab5b9a53c30633bf3e3f6094fdb7927a20
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2019-11-07 02:29:55 +0100

    Created a Master model

---
+++ api/sms/models/master.py
@@ -0,0 +1,14 @@
+from sms.config import db, ma
+
+
+class Master(db.Model):
+    __bind_key__ = 'master'
+    __tablename__ = 'Main'
+    mat_no = db.Column('MATNO', db.String(10), primary_key=True, unique=True)
+    database = db.Column('DATABASE', db.String(12), nullable=False)
+
+
+class MasterSchema(ma.ModelSchema):
+    class Meta:
+        model = Master
+        sqla_session = db.session
