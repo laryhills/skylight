9a2b5586fb649f02e2bf6394ccf222aaf89d14cd
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2019-11-07 02:28:44 +0100

    Update config.py

--- api/sms/config.py
+++ api/sms/config.py
@@ -2,6 +2,7 @@ import os
 import secrets
 import connexion
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 
 base_dir = os.path.dirname(__file__)
@@ -12,8 +13,11 @@ app = connex_app.app
 
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(db_path, 'accounts.db')
+app.config['SQLALCHEMY_BINDS'] = {'master': 'sqlite:///' + os.path.join(db_path, 'master.db'),
+                                  'courses': 'sqlite:///' + os.path.join(db_path, 'courses.db'),
+                                  '2015-2016': 'sqlite:///' + os.path.join(db_path, '2015-2016.db')}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
-
+ma = Marshmallow(app)
 bcrypt = Bcrypt(app)
