e2b9844bc5975a24c0190908e773cf3fa09768d1
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-01-08 10:18:27 +0100

    Added the result models

--- scripts/generate_models.py
+++ scripts/generate_models.py
@@ -6,7 +6,8 @@ for session in sessions:
     model = open(os.path.join(models_dir, '_{0}_{1}.py'.format(session, session + 1)), 'w')
     temp_file = open('model_template.txt')
     lines = temp_file.readlines()
-    lines[4] = lines[4].replace('##NAME##', '{0}-{1}'.format(session, session + 1))
+    for index in range(len(lines)):
+        lines[index] = lines[index].replace('##NAME##', '{0}-{1}'.format(session, session + 1))
     model.writelines(lines)
 
 print('Done')
