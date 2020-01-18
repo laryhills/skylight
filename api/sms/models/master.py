33ad13f33409b59d0d2be8fd9089d22f08e78955
lordfme <fortmax120@gmail.com>
2020-01-18 13:07:14 +0100

    removed unique keyword, redundant in pri key

--- api/sms/models/master.py
+++ api/sms/models/master.py
@@ -4,9 +4,8 @@ from sms.config import db, ma
 class Master(db.Model):
     __bind_key__ = 'master'
     __tablename__ = 'Main'
-    mat_no = db.Column('MATNO', db.String(10), primary_key=True, unique=True)
+    mat_no = db.Column('MATNO', db.String(10), primary_key=True)
     database = db.Column('DATABASE', db.String(12), nullable=False)
-
 
 class MasterSchema(ma.ModelSchema):
     class Meta:
