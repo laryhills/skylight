5265338f391ed38d0082dde15d659d8389f51f82
lordfme <fortmax120@gmail.com>
2020-01-19 11:15:24 +0100

    Allow for handling different sessions with import reload

--- api/sms/result_poll.py
+++ api/sms/result_poll.py
@@ -1,13 +1,21 @@
+from sys import modules
 from sms.config import db
 from sms import master_poll
+from importlib import reload
 
 lastLoaded = None
 
 def get(mat_no, level=None):
+    #Get db file for student
     db_name = master_poll.getDB(mat_no)[:-3]
-
-    ans='['
+    #Import model and force import override if necessary (session changes)
+    global lastLoaded
     exec('from sms.models import _{}'.format(db_name))
+    if ('sms.models._{}'.format(db_name) in modules) and (lastLoaded!=db_name):
+        exec('reload(_{})'.format(db_name))
+    lastLoaded = db_name
+    #Get result for all levels if none else for level
+    ans='['
     levels = [level] if level else range(100,900,100)
     for level in levels:
         resLvl = eval('_{}.Result{}.query.filter_by(mat_no=mat_no).first()'.format(db_name,level))
