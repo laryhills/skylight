a67c2fe14c2f7655894c2a6e5de19903708b72d9
lordfme <fortmax120@gmail.com>
2020-01-19 09:41:37 +0100

    Result poll fleshed out API

--- api/sms/result_poll.py
+++ api/sms/result_poll.py
@@ -1,33 +1,16 @@
-from flask import abort
 from sms.config import db
-from json import loads
-from sms.models.master import Master, MasterSchema
-#from sms.models._2013_2014 import *
+from sms import master_poll
 
-def get(mat_no=None, level=None):
-    if mat_no:
-        # Lookup the student's details in the master db
-        student = Master.query.filter_by(mat_no=mat_no).first_or_404()
-        master_schema = MasterSchema()
-        db_name = master_schema.dump(student)['database']
-        db_name = db_name.replace('-', '_')
+lastLoaded = None
 
-        ans=[]
-        exec('from sms.models._{} import *'.format(db_name[:-3]))
-        print (locals()['Result100'].__bind_key__)
-        if level == None:
-            for level in range(100,900,100):
-                try:
-                    resLvl = eval('Result{}.query.filter_by(mat_no=mat_no).first()'.format(level))
-                    resStr = eval('Result{}Schema().dumps(resLvl)'.format(level))
-                    ans.append(loads(resStr))
-                except Exception:
-                    ans.append({})
-        else:
-            try:
-                resLvl = eval('Result{}.query.filter_by(mat_no=mat_no).first()'.format(level))
-                resStr = eval('Result{}Schema().dumps(resLvl)'.format(level))
-                ans.append(loads(resStr))
-            except Exception:
-                ans.append({})
-        print(ans)
+def get(mat_no, level=None):
+    db_name = master_poll.getDB(mat_no)[:-3]
+
+    ans='['
+    exec('from sms.models import _{}'.format(db_name))
+    levels = [level] if level else range(100,900,100)
+    for level in levels:
+        resLvl = eval('_{}.Result{}.query.filter_by(mat_no=mat_no).first()'.format(db_name,level))
+        resStr = eval('_{}.Result{}Schema().dumps(resLvl)'.format(db_name,level))
+        ans+=(resStr+',')
+    return ans[:-1]+']'
