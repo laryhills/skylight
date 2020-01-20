5e5ca50f18d06e101338bf5df6dfbfb85be91ff1
lordfme <fortmax120@gmail.com>
2020-01-20 23:11:35 +0100

    removed global call for efficiency

--- api/sms/course_details.py
+++ api/sms/course_details.py
@@ -1,18 +1,11 @@
 from sms.config import db
 from sms.models.courses import CoursesSchema
 
-Courses = None
-
-
 def get(course_code):
-    global Courses
     level = int(course_code[3]) if course_code[:3] != 'CED' else 4
     exec('from sms.models.courses import Courses{} as Courses'.format(level * 100))
-    Courses = locals()['Courses']
-    course = Courses.query.filter_by(course_code=course_code).first_or_404()
-    course_schema = CoursesSchema()
-
-    return course_schema.dumps(course)
+    course = eval('Courses').query.filter_by(course_code=course_code).first_or_404()
+    return CoursesSchema().dumps(course)
 
 
 def post(course):
