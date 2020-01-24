dc2cd9697e84454357bb9361252688306d3a8d74
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-01-25 00:05:05 +0100

    Edited file to comply with PEP8 standard

--- api/sms/result_statement.py
+++ api/sms/result_statement.py
@@ -1,14 +1,15 @@
 from sms import result_poll
 from sms import personal_info
 from sms import course_details
-from json import loads,dumps
+from json import loads, dumps
+
 
 def get(mat_no):
     person = loads(personal_info.get(mat_no=mat_no))
     
-    student_details = {"name":"{}, {}".format(person['surname'],person['othernames']), "depat":"MECHANICAL ENGINEERING",
-                       "dob":person['date_of_birth'], "mode_of_entry":person['mode_of_entry'], "results":[], "credits":[],
-                       "entry_session":person['session_admitted'], "grad_session":person['session_grad']}
+    student_details = {"name": "{}, {}".format(person['surname'], person['othernames']), "depat": "MECHANICAL ENGINEERING",
+                       "dob": person['date_of_birth'], "mode_of_entry": person['mode_of_entry'], "results": [], "credits": [],
+                       "entry_session": person['session_admitted'], "grad_session": person['session_grad']}
     
     results = loads(result_poll.get(mat_no))
     finalResults = []
@@ -20,13 +21,13 @@ def get(mat_no):
             category = result.pop('category')
             if lvl:
                 carryovers = result.pop('carryovers')
-                if (carryovers != "nan, F"):
+                if carryovers != "nan, F":
                     carryovers = carryovers.split(',')
                     for co in carryovers:
                         (course, score, grade) = co.split()
-                        result[course] = ",".join([score,grade])
+                        result[course] = ",".join([score, grade])
             credits_passed = credits_failed = credits_total = 0
-            lvlResult={"first_sem":[],"second_sem":[]}
+            lvlResult = {"first_sem": [], "second_sem": []}
             for course in result:
                 if result[course]:
                     course_props = loads(course_details.get(course))
@@ -35,18 +36,19 @@ def get(mat_no):
                     title = course_props['course_title']
                     (score, grade) = [x.strip() for x in result[course].split(',')]
 
-                    if grade=='F':
+                    if grade == 'F':
                         credits_failed += credit
                     else:
-                        credits_passed +=credit
+                        credits_passed += credit
                     credits_total += credit
 
-                    lvlResult[["first_sem","second_sem"][sem-1]].append(((lvl+1)*100,course,title,credit,int(score),grade))
+                    lvlResult[["first_sem", "second_sem"][sem-1]].append(((lvl+1)*100,course,title,credit,int(score),grade))
 
             finalResults.append(lvlResult)
             student_details["credits"].append((credits_total,credits_passed,credits_failed))
     student_details["results"] = finalResults
     return dumps(student_details)
+
 
 def get_carryovers(mat_no):
     first_sem, second_sem, carryovers = {}, {}, {}
