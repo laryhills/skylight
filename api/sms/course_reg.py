ac9f0edf4118fbb8ea32c52d09c894f5fe0a8ece
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-01-28 02:31:46 +0100

    Updated course_reg.py

--- api/sms/course_reg.py
+++ api/sms/course_reg.py
@@ -1,19 +1,25 @@
+import os
+import secrets
 from flask import render_template
 from weasyprint import HTML
 from sms.config import app, base_dir
-from sms.result_statement import get_carryovers
-from sms import personal_info
+from sms.utils import get_carryovers
+from sms import generated_pdfs, personal_info
 from json import loads
 
 
 def get(mat_no, session):
+    if mat_no in generated_pdfs:
+        uri = os.path.join(os.path.expanduser('~'), 'sms', 'cache', generated_pdfs[mat_no])
+        return uri
+
     person = loads(personal_info.get(mat_no))
     level = list(str(person['current_level']))
     phone_no = list(person['phone_no'])
     mode_of_entry = 'PUTME' if person['mode_of_entry'] == 1 else ['DE(200)', 'DE(300)'][person['mode_of_entry'] == 3]
     sex = ['Female', 'Male'][person['sex'] == 'M']
 
-    carryovers = loads(get_carryovers(mat_no))
+    carryovers = loads(get_carryovers(mat_no, person['current_level']))
     first_sem = carryovers['first_sem']
     if first_sem:
         first_sem_carryover_courses, first_sem_carryover_credits = list(zip(*first_sem))
@@ -34,4 +40,8 @@ def get(mat_no, session):
                                first_sem_carryover_credits=first_sem_carryover_credits,
                                second_sem_carryover_courses=second_sem_carryover_courses,
                                second_sem_carryover_credits=second_sem_carryover_credits)
-        HTML(string=html).write_pdf('course_form.pdf')
+        file_name = secrets.token_hex(8) + '.pdf'
+        uri = os.path.join(os.path.expanduser('~'), 'sms', 'cache', file_name)
+        HTML(string=html).write_pdf(uri)
+        generated_pdfs[mat_no] = file_name
+        return uri
