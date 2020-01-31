e179f9d034afdda5e23b0a0a3454a358a015036b
lordfme <fortmax120@gmail.com>
2020-01-31 06:29:30 +0100

    Gen sessional models till _2018_2019

--- scripts/generate_models.py
+++ scripts/generate_models.py
@@ -1,7 +1,7 @@
 import os
 
 models_dir = os.path.join(os.path.dirname(__file__), '..', 'api', 'sms', 'models')
-sessions = range(2003, 2018)
+sessions = range(2003, 2019)
 for session in sessions:
     model = open(os.path.join(models_dir, '_{0}_{1}.py'.format(session, session + 1)), 'w')
     temp_file = open('model_template.txt')
