79bf7a1e57c2b0b994270024d161390daf560462
lordfme <fortmax120@gmail.com>
2020-01-31 06:28:03 +0100

    Placed GST Courses for DE in correct sem

--- scripts/generate_courses_&_credits.py
+++ scripts/generate_courses_&_credits.py
@@ -42,7 +42,8 @@ def generate_courses_and_credits_table(conn, session):
     de_level_courses, de_credits = level_courses[:], credits[:]
     de_credits[0], de_level_courses[0] = None, None
     de_credits[1] += 10
-    de_level_courses[1] += ',GST111,GST112,GST121,GST122,GST123'
+    de_level_courses[0] += 'GST111,GST112'
+    de_level_courses[1] += 'GST121,GST122,GST123'
     cursor.execute('INSERT INTO Courses VALUES (?, ?, ?, ?, ?, ?);', [2]+de_level_courses)
     cursor.execute('INSERT INTO Credits VALUES (?, ?, ?, ?, ?, ?);', [2]+de_credits)
     conn.commit()
