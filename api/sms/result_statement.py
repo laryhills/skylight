f480b7e08f7ec40e7466c5a089b994a94f4aa632
lordfme <fortmax120@gmail.com>
2020-02-06 23:10:20 +0100

    added catg field to res_stmt, retJSON param, default (None) carryover field, handle abs rsult as failedr

--- api/sms/result_statement.py
+++ api/sms/result_statement.py
@@ -5,12 +5,12 @@ from sms import course_details
 from json import loads, dumps
 
 
-def get(mat_no):
+def get(mat_no, retJSON=True):
     person = loads(personal_info.get(mat_no=mat_no))
     
     student_details = {"name": "{}, {}".format(person['surname'], person['othernames']), "depat": "MECHANICAL ENGINEERING",
                        "dob": person['date_of_birth'], "mode_of_entry": person['mode_of_entry'], "results": [], "credits": [],
-                       "entry_session": person['session_admitted'], "grad_session": person['session_grad']}
+                       "category": [], "entry_session": person['session_admitted'], "grad_session": person['session_grad']}
     
     results = loads(result_poll.get(mat_no))
     finalResults = []
@@ -22,33 +22,47 @@ def get(mat_no):
             category = result.pop('category')
             if lvl:
                 carryovers = result.pop('carryovers')
-                if carryovers != "0,F":
+                if carryovers != "-1,ABS":
                     carryovers = carryovers.split(',')
                     for co in carryovers:
+                        coSplit = co.split()
+                        # Special cases, del on fix DB
+                        if len(coSplit)==4:
+                            coSplit.remove('nan')
+                            co = " ".join(coSplit)
+                        if len(coSplit) == 2:
+                            co = " ".join(coSplit+["F"])
                         (course, score, grade) = co.split()
                         result[course] = ",".join([score, grade])
             credits_passed = credits_failed = credits_total = 0
             lvlResult = {"first_sem": [], "second_sem": []}
             for course in result:
                 if result[course]:
-                    course_props = loads(course_details.get(course))
-                    sem = course_props['course_semester']
-                    credit = course_props['course_credit']
-                    title = course_props['course_title']
-                    (score, grade) = [x.strip() for x in result[course].split(',')]
+                    if course == "CHM112":
+                        # Manually deal with this typo Special cases, del on fix DB
+                        course, sem, credit, title = "CHM122", 2, 3, "General Chemistry II"
+                        (score, grade) = [x.strip() for x in result["CHM112"].split(',')]
+                    else:
+                        course_props = loads(course_details.get(course))
+                        sem = course_props['course_semester']
+                        credit = course_props['course_credit']
+                        title = course_props['course_title']
+                        (score, grade) = [x.strip() for x in result[course].split(',')]
 
-                    if grade == 'F':
+                    if grade == 'F' or grade == 'ABS':
                         credits_failed += credit
                     else:
                         credits_passed += credit
                     credits_total += credit
-
                     lvlResult[["first_sem", "second_sem"][sem-1]].append(((lvl+1)*100,course,title,credit,int(score),grade))
 
             finalResults.append(lvlResult)
             student_details["credits"].append((credits_total,credits_passed,credits_failed))
+            student_details["category"].append(category)
     student_details["results"] = finalResults
-    return dumps(student_details)
+    if retJSON:
+        return dumps(student_details)
+    return student_details
 
 
 def get_gpa(mat_no):
