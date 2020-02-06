b053e797b98550e224bfd870c0e6e89a06d57e7d
lordfme <fortmax120@gmail.com>
2020-02-06 21:45:04 +0100

    Added retJSON param to course_details

--- api/sms/course_details.py
+++ api/sms/course_details.py
@@ -1,11 +1,13 @@
 from sms.config import db
 from sms.models.courses import CoursesSchema
 
-def get(course_code):
+def get(course_code, retJSON=True):
     level = int(course_code[3]) if course_code[:3] != 'CED' else 4
     exec('from sms.models.courses import Courses{} as Courses'.format(level * 100))
     course = eval('Courses').query.filter_by(course_code=course_code).first_or_404()
-    return CoursesSchema().dumps(course)
+    if retJSON:
+        return CoursesSchema().dumps(course)
+    return CoursesSchema().dump(course)
 
 
 def post(course):
