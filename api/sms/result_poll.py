22e8cebc4af41cee5075492f494c429374799500
lordfme <fortmax120@gmail.com>
2020-02-07 04:27:21 +0100

    refactored, used util.load_session for sessional model import

--- api/sms/result_poll.py
+++ api/sms/result_poll.py
@@ -1,25 +1,15 @@
 from json import dumps
-from sys import modules
-from sms.config import db
 from sms import utils
-from importlib import reload
 
-lastLoaded = None
 
 def get(mat_no, level=None):
-    #Get db file for student
-    db_name = utils.get_DB(mat_no)[:-3]
-    #Import model and force import override if necessary (session changes)
-    global lastLoaded
-    exec('from sms.models import _{}'.format(db_name))
-    if ('sms.models._{}'.format(db_name) in modules) and (lastLoaded!=db_name):
-        exec('reload(_{})'.format(db_name))
-    lastLoaded = db_name
     #Get result for all levels if none else for level
+    db_name = utils.get_DB(mat_no)[:-3]
+    session = utils.load_session(db_name)
     ans=[]
     levels = [level] if level else range(100,900,100)
     for level in levels:
-        resLvl = eval('_{}.Result{}.query.filter_by(mat_no=mat_no).first()'.format(db_name,level))
-        resStr = eval('_{}.Result{}Schema().dump(resLvl)'.format(db_name,level))
+        resLvl = eval('session.Result{}.query.filter_by(mat_no=mat_no).first()'.format(level))
+        resStr = eval('session.Result{}Schema().dump(resLvl)'.format(level))
         ans.append(resStr)
     return dumps(ans)
