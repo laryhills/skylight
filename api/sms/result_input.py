e84b83ca4c9877cac75817e1a81cda9741defacb
chima <chimaataman@gmail.com>
2020-02-24 14:17:27 +0100

    script to handle result input

---
+++ api/sms/result_input.py
@@ -0,0 +1,25 @@
+'''
+The result format is as below
+ res300 = results[2]
+>>> res300
+{'first_sem': [(300, 'PRE311', 'Manufacturing Technology III', 2, 64, 'B'),
+               (300, 'MEE311', 'Mechanics of Machines I', 3, 78, 'A'),
+               (300, 'ELA301', 'Laboratory III', 2, 70, 'A'),
+               (300, 'EMA381', 'Engineering Mathematics III', 3, 73, 'A'),
+               (300, 'MEE321', 'Engineering Drawing III', 3, 83, 'A'),
+               (300, 'MEE351', 'Thermodynamics I', 2, 81, 'A'),
+               (300, 'MEE361', 'Fluid Mechanics I', 2, 72, 'A'),
+               (300, 'CVE311', 'Theory of Structures & Strength of Materials II', 3, 80, 'A'),
+               (300, 'EEE317', 'Electrical Engineering III', 3, 83, 'A')],
+
+'second_sem': [(300, 'EMA382', 'Engineering Mathematics IV', 4, 68, 'B'),
+               (300, 'MEE362', 'Fluid Mechanics II', 2, 92, 'A'),
+               (300, 'MEE312', 'Mechanics of Machines II', 3, 80, 'A'),
+               (300, 'MEE322', 'Creative Problem Solving', 3, 68, 'B'),
+               (300, 'MEE332', 'Strength of Materials III', 2, 81, 'A'),
+               (300, 'MEE372', 'Engineering Computers Graphics', 1, 50, 'C'),
+               (300, 'EEE318', 'Electrical Engineering IV', 2, 64, 'B'),
+               (300, 'MEE342', 'Materials Science & Production Processes', 2, 86, 'A'),
+               (300, 'ELA302', 'Laboratory IV', 2, 58, 'C'),
+               (300, 'MEE352', 'Engineering Thermodynamics II', 2, 61, 'B')]}
+'''
