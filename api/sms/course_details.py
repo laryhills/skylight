0a4dba32378550af51f1ab2a1397296cb3e3e5a1
lordfme <fortmax120@gmail.com>
2020-03-05 18:56:44 +0100

    removed get method of course_reg from being exposed, moved fn perms to users.py

--- api/sms/course_details.py
+++ api/sms/course_details.py
@@ -1,10 +1,8 @@
 from sms.config import db
 from sms.models.courses import CoursesSchema
-from sms.users import access_decorator
 
 
-@access_decorator
-def get(course_code, retJSON=True, req_perms=["read"]):
+def get(course_code, retJSON=True):
     level = int(course_code[3]) if course_code[:3] != 'CED' else 4
     exec('from sms.models.courses import Courses{} as Courses'.format(level * 100))
     course = eval('Courses').query.filter_by(course_code=course_code).first_or_404()
@@ -14,7 +12,7 @@ def get(course_code, retJSON=True, req_perms=["read"]):
 
 
 @access_decorator
-def post(course, req_perms=["superuser"]):
+def post(course):
     course_schema = CoursesSchema()
     course = course_schema.load(course)
     db.session.add(course)
