6f6862952c5e7269e9db38e9410ab13b13e449b5
lordfme <fortmax120@gmail.com>
2020-03-06 13:05:12 +0100

    Added ID pri key to logs model

--- api/sms/models/logs.py
+++ api/sms/models/logs.py
@@ -4,7 +4,8 @@ from sms.config import db, ma
 class Logs(db.Model):
     __bind_key__ = 'logs'
     __tablename__ = 'Logs'
-    user = db.Column('USER', db.Text, nullable=False, primary_key=True)
+    id = db.Column('ID', db.Integer, nullable=False, primary_key=True)
+    user = db.Column('USER', db.Text, nullable=False)
     operation = db.Column('OPERATION', db.Text, nullable=False)
     params = db.Column('PARAMS', db.Text, nullable=True)
     timestamp = db.Column('TIMESTAMP', db.Integer, nullable=False)
