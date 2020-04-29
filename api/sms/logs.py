a585f6cc5b9daeb469d03542c628bab1c25c9834
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-04-29 18:37:29 +0100

    Modified the get function to always return the most recent logs

--- api/sms/logs.py
+++ api/sms/logs.py
@@ -5,5 +5,9 @@ from json import loads
 
 @access_decorator
 def get(limit = 5, offset = 0):
-    log_list = Logs.query.offset(offset*limit).limit(limit).all()
+    log_size = Logs.query.count()
+    offset = log_size - limit - offset
+    if offset < 0:
+        return [[]]
+    log_list = Logs.query.offset(offset).limit(limit).all()
     return [(log.timestamp, fn_props[log.operation]["logs"](log.user, loads(log.params))) for log in log_list]
