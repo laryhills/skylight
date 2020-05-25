31d56bf91f683775528fcd33269e63dee2586da6
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-05-25 14:41:09 +0100

    Updated logs.py

--- api/sms/logs.py
+++ api/sms/logs.py
@@ -5,10 +5,14 @@ from json import loads
 
 # todo: implement access controls w/o logging
 # @access_decorator
-def get(limit = 5, offset = 0):
+def get(limit=5, offset=0):
     log_size = Logs.query.count()
     offset = log_size - limit - offset
     if offset < 0:
-        return [[]]
+        if limit == 15:
+            # Acts as an EOF for the scrolling effect in the frontend which sends a query with limit = 15
+            return [[]]
+        else:
+            offset = 0
     log_list = Logs.query.offset(offset).limit(limit).all()
     return [(log.timestamp, fn_props[log.operation]["logs"](log.user, loads(log.params))) for log in log_list]
