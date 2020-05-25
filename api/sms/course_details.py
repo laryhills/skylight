07959e33f81f48e8fe5c4afa52ffd1158f5b3db8
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-05-25 14:40:02 +0100

    Updated the course_details endpoint

--- api/sms/course_details.py
+++ api/sms/course_details.py
@@ -1,6 +1,10 @@
-from sms.config import db
+from sms.config import db, get_current_session
 from sms.models.courses import CoursesSchema
-from sms.users import access_decorator
+from sms.users import access_decorator, load_session
+
+# TODO Create endpoint for teaching departments
+# TODO Change primary key of all courses models from it's course_code to an id
+# As it stands, a course's code can't be modified
 
 
 def get(course_code, retJSON=True):
@@ -12,9 +16,67 @@ def get(course_code, retJSON=True):
     return CoursesSchema().dump(course)
 
 
+def get_course_details(course_code=None, level=None, use_curr_session=True):
+    if course_code:
+        return get_by_course_code(course_code)
+    else:
+        return get_all(level, use_curr_session=use_curr_session)
+
+
+@access_decorator
+def get_by_course_code(course_code):
+    return [get(course_code, retJSON=False)], 200
+
+
+@access_decorator
+def get_all(level, use_curr_session=True):
+    if use_curr_session:
+        # curr_session = get_current_session()
+        curr_session = 2018     # Change this when 2019 session data is available
+        model = load_session('{}_{}'.format(curr_session, curr_session + 1))
+        course_codes = model.Courses.query.filter_by(mode_of_entry=1).first()
+        level_courses = model.CoursesSchema().dump(course_codes)['level' + str(level)]
+        courses = []
+        for course_code in level_courses.replace(' ', ',').split(','):
+            courses.append(get(course_code, retJSON=False))
+        return courses, 200
+    else:
+        exec('from sms.models.courses import Courses{} as Courses'.format(level))
+        courses = eval('Courses').query.all()
+        return CoursesSchema(many=True).dump(courses), 200
+
+
 @access_decorator
 def post(course):
-    course_schema = CoursesSchema()
-    course = course_schema.load(course)
-    db.session.add(course)
+    course_level = course['course_level']
+    exec('from sms.models.courses import Courses{} as Courses'.format(course_level))
+    model = eval('Courses')
+    course_obj = model(**course)
+    db.session.add(course_obj)
+    db.session.commit()
+
+
+@access_decorator
+def put(data):
+    error_log = []
+    for course in data:
+        course_level = course['course_level']
+        exec('from sms.models.courses import Courses{} as Courses'.format(course_level))
+        course_obj = eval('Courses').query.filter_by(course_code=course['course_code']).first()
+        if not course_obj:
+            msg = course['course_code'] + 'not found'
+            error_log.append(msg)
+            continue
+        for k, v in course.items():
+            setattr(course_obj, k, v)
+        db.session.add(course_obj)
+    db.session.commit()
+    return error_log, 200
+
+
+@access_decorator
+def delete(course_code, course_level):
+    exec('from sms.models.courses import Courses{} as Courses'.format(course_level))
+    course_obj = eval('Courses').query.filter_by(course_code=course_code).first_or_404()
+    db.session.delete(course_obj)
     db.session.commit()
