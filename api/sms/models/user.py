096a7030ea6d33d82fe1a82926e46b28120113f2
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-05-25 14:35:14 +0100

    Added 3 new fields to the 'User' model. Also created a marshmallow schema for the model

--- api/sms/models/user.py
+++ api/sms/models/user.py
@@ -1,8 +1,18 @@
-from sms.config import db
+from sms.config import db, ma
 
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    firstname = db.Column(db.String(90), unique=False, nullable=False)
+    lastname = db.Column(db.String(90), unique=False, nullable=False)
+    email = db.Column(db.String(90), nullable=False)
     username = db.Column(db.String(32), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     permissions = db.Column(db.Text, nullable=False)
+
+
+class UserSchema(ma.ModelSchema):
+    class Meta:
+        model = User
+        exclude = ('password',)
+        sqla_session = db.session
