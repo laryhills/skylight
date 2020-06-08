85ce3b8ac25979fac35627f9309bd9d4cb6947ee
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-06-08 04:59:03 +0100

    Added the `senate_version.get` method to the `fn_props` dict

--- api/sms/users.py
+++ api/sms/users.py
@@ -214,4 +214,7 @@ fn_props = {
     "accounts.delete": {"perms": ["superuser", "write"],
                         "logs": lambda user, params: "{} deleted an account with id {}".format(user, params.get('uid'))
                      },
+    "senate_version.get": {"perms": ["superuser", "write"],
+                           "logs": lambda user, params: "{} requested for the senate version for the {} session".format(user, params.get('acad_session'))
+                     },
 }
