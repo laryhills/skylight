07bd48e989de10d3d1322bea2ba41f2ae742076d
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-06-08 04:56:02 +0100

    Created the `Category` model

--- api/sms/models/master.py
+++ api/sms/models/master.py
@@ -7,6 +7,17 @@ class Master(db.Model):
     mat_no = db.Column('MATNO', db.String(10), primary_key=True)
     database = db.Column('DATABASE', db.String(12), nullable=False)
 
+
+class Category(db.Model):
+    __bind_key__ = 'master'
+    __tablename__ = 'Category'
+    category = db.Column(db.String(1), primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.String(500))
+    headers = db.Column(db.String(100))
+    sizes = db.Column(db.String(100))
+
+
 class MasterSchema(ma.ModelSchema):
     class Meta:
         model = Master
