4856938bc955c36a7b49a12c1b82dc03569d551d
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-06-26 01:54:02 +0100

    Script for generating the `DegreeClass` tables

---
+++ scripts/generate_degree_class.py
@@ -0,0 +1,34 @@
+import os
+import sqlite3
+
+start_session = 2003
+curr_session = 2019
+db_base_dir = os.path.join(os.path.dirname(__file__), '..', 'api', 'sms', 'database')
+
+class_of_degree = {
+    'First class honours': '4.50,5.00',
+    'Second class honours (upper)': '3.50,4.49',
+    'Second class honours (lower)': '2.40,3.49',
+    'Third class honours': '1.50,2.39',
+    'Pass': '1.00,1.49',
+    'Fail': '0,0.99'
+}
+
+
+def generate_db():
+    for session in range(start_session, curr_session + 1):
+        curr_db = '{}-{}.db'.format(session, session + 1)
+        conn = sqlite3.connect(os.path.join(db_base_dir, curr_db))
+        stmt = 'CREATE TABLE DegreeClass(Class PRIMARY_KEY TEXT, Limits TEXT);'
+        conn.execute(stmt)
+        stmt = 'INSERT INTO DegreeClass VALUES (?, ?);'
+        for cls, limit in class_of_degree.items():
+            conn.execute(stmt, (cls, limit))
+        conn.commit()
+        conn.close()
+
+
+if __name__ == '__main__':
+    generate_db()
+
+    print('Done')
