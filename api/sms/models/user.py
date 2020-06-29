1f3178b7ca248d5d00cb3d11aa913ad92f89cb11
lordfme <fortmax120@gmail.com>
2020-06-29 18:59:55 +0100

    Updated accounts db columns and model file

--- api/sms/models/user.py
+++ api/sms/models/user.py
@@ -2,13 +2,13 @@ from sms.config import db, ma
 
 
 class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    firstname = db.Column(db.String(90), unique=False, nullable=False)
-    lastname = db.Column(db.String(90), unique=False, nullable=False)
-    email = db.Column(db.String(90), nullable=False)
-    username = db.Column(db.String(32), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.Text, primary_key=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
     permissions = db.Column(db.Text, nullable=False)
+    role = db.Column(db.Text, unique=True, nullable=False)
+    fullname = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False)
+
 
 
 class UserSchema(ma.ModelSchema):
