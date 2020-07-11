7cf73584f3800df04733f72ada546521cde2bdac
lordfme <fortmax120@gmail.com>
2020-07-11 17:28:15 +0100

    allowed for getting single account details, added return server error codes

--- api/sms/accounts.py
+++ api/sms/accounts.py
@@ -4,11 +4,13 @@ from sms.models.user import User, UserSchema
 
 
 @accounts_decorator
-def get():
-    all_users = User.query.all()
-    user_schema = UserSchema(many=True)
+def get(username=None):
     accounts = []
-    for user in user_schema.dump(all_users):
+    if username == None:
+        users = User.query.all()
+    else:
+        users = [User.query.filter_by(username=username).first()]
+    for user in UserSchema(many=True).dump(users):
         accounts.append(user)
     return accounts, 200
 
@@ -16,12 +18,14 @@ def get():
 @accounts_decorator
 def post(data):
     # TODO not recv this in plain-text
-    password = data.pop('password')
-    user_schema = UserSchema()
-    new_user = user_schema.load(data)
-    new_user.password = bcrypt.generate_password_hash(password)
+    hashed_password = bcrypt.generate_password_hash(data["password"]).decode("utf-8")
+    data["password"] = hashed_password
+    new_user = UserSchema().load(data)
     db.session.add(new_user)
-    db.session.commit()
+    try:
+        db.session.commit()
+    except:
+        return None, 500
     return None, 200
 
 
@@ -31,7 +35,10 @@ def put(data):
     # TODO not recv password in plain text, do decode here
     data['password'] = bcrypt.generate_password_hash(password)
     User.query.filter_by(username=username).update(data)
-    db.session.commit()
+    try:
+        db.session.commit()
+    except:
+        return None, 500
     return None, 200
 
 
@@ -43,7 +50,10 @@ def manage(data):
     if "permissions" in data:
         data.pop("permissions")
     User.query.filter_by(username=username).update(data)
-    db.session.commit()
+    try:
+        db.session.commit()
+    except:
+        return None, 500
     return None, 200
 
 
@@ -51,5 +61,8 @@ def manage(data):
 def delete(username):
     user = User.query.filter_by(username=username).first()
     db.session.delete(user)
-    db.session.commit()
+    try:
+        db.session.commit()
+    except:
+        return None, 500
     return None, 200
