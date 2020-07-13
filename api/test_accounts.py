f17f6d91778bcbb3e7411547d9df5deed7899508
lordfme <fortmax120@gmail.com>
2020-07-13 12:05:56 +0100

    Added put and manage tests to test script

--- api/test_accounts.py
+++ api/test_accounts.py
@@ -52,3 +52,48 @@ def test_delete_account():
     output, ret_code = accounts.delete(username="delete_test")
     assert ret_code == 200
     assert cur.execute("SELECT * FROM user WHERE username=?",("delete_test",)).fetchone() == None
+
+
+def test_put_account():
+    dummy_acct = {
+            'email': 'test@dummy.com',
+            'permissions': '{"read": false, "write": false, "superuser": false, "levels": [], "usernames": []}',
+            'fullname': 'Skylight Testing',
+            'title': 'Tester',
+            'password': 'testing',
+            'username': 'put_test',
+    }
+    cur.execute("INSERT INTO user VALUES(?,?,?,?,?,?)",(dummy_acct["username"], "somepwdhash", "{}", "Put Test", "Putin Vlad", "vlad@put.in"))
+    conn.commit()
+    output, ret_code = accounts.put(data=dummy_acct)
+    assert ret_code == 200
+    user_row = cur.execute("SELECT * FROM user WHERE username=?",(dummy_acct["username"],)).fetchone()
+    dummy_acct.pop("password")
+    for prop in dummy_acct:
+        assert dummy_acct[prop] == user_row[prop]
+    cur.execute("DELETE FROM user WHERE username=?",(dummy_acct["username"],))
+    conn.commit()
+
+
+def test_manage_account():
+    dummy_acct = {
+            'email': 'test@dummy.com',
+            'permissions': '{"read": false, "write": false, "superuser": false, "levels": [], "usernames": []}',
+            'fullname': 'Skylight Testing',
+            'title': 'Tester',
+            'password': 'testing',
+            'username': 'manage_test',
+    }
+    cur.execute("INSERT INTO user VALUES(?,?,?,?,?,?)",(dummy_acct["username"], "somepwdhash", "{}", "Manage Test", "Mana Ger", "Mana@g.er"))
+    conn.commit()
+    output, ret_code = accounts.manage(data=dummy_acct)
+    assert ret_code == 200
+    user_row = cur.execute("SELECT * FROM user WHERE username=?",(dummy_acct["username"],)).fetchone()
+    dummy_acct.pop("password")
+    for prop in dummy_acct:
+        if prop != "permissions":
+            assert dummy_acct[prop] == user_row[prop]
+        else:
+            assert user_row[prop] == "{}"
+    cur.execute("DELETE FROM user WHERE username=?",(dummy_acct["username"],))
+    conn.commit()
