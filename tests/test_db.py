fceca2b3c524da76515b24ce23127e9b9c82fb69
lordfme <fortmax120@gmail.com>
2020-08-14 11:16:42 +0100

    added tcp and tcr db tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,6 @@
+import re
 from sms.src import personal_info
+from sms.src import course_details
 from sms.models.master import Master
 from sms.src.utils import get_DB, load_session
 
@@ -35,3 +37,47 @@ def test_levels():
     pass #for student in Master.query.all():
     pass #    db_name = get_DB(student.mat_no)
     pass #    student_info = personal_info.get(student.mat_no)
+
+
+def test_tcr():
+    for student in Master.query.all():
+        mat_no = student.mat_no
+        print (mat_no)
+        session = load_session(student.database)
+        # TODO use new col to know table to stop
+        for level in range(100, 900, 100):
+            course_reg_lvl = eval("session.CourseReg{}".format(level))
+            course_reg = course_reg_lvl.query.filter_by(mat_no=mat_no).first()
+            if course_reg:
+                expected_TCR = 0
+                for prop in dir(course_reg):
+                    if re.match("[A-Z][A-Z][A-Z][0-9][0-9][0-9]", prop):
+                        if int(course_reg.__getattribute__(prop)):
+                            expected_TCR += course_details.get(prop,0)["course_credit"]
+                for course in course_reg.carryovers.split(","):
+                    if re.match("[A-Z][A-Z][A-Z][0-9][0-9][0-9]", course):
+                        expected_TCR += course_details.get(course,0)["course_credit"]
+                assert expected_TCR == course_reg.tcr
+
+
+def test_tcp():
+    for student in Master.query.all():
+        mat_no = student.mat_no
+        print (mat_no)
+        session = load_session(student.database)
+        # TODO use new col to know table to stop
+        for level in range(100, 900, 100):
+            result_lvl = eval("session.Result{}".format(level))
+            result = result_lvl.query.filter_by(mat_no=mat_no).first()
+            if result:
+                expected_TCP = 0
+                for prop in dir(result):
+                    if re.match("[A-Z][A-Z][A-Z][0-9][0-9][0-9]", prop) and result.__getattribute__(prop):
+                        grade = result.__getattribute__(prop).split(",")[1]
+                        if grade not in ("F", "ABS"):
+                            expected_TCP += course_details.get(prop,0)["course_credit"]
+                if result.carryovers:
+                    for course, score, grade in [x.split() for x in result.carryovers.split(",")]:
+                        if grade not in ("F", "ABS"):
+                            expected_TCP += course_details.get(course,0)["course_credit"]
+                assert expected_TCP == result.tcp
