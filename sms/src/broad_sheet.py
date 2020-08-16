2cab1925cf6c432a2a0420402aeafc7cf18e841d
chima <chimaataman@gmail.com>
2020-08-16 21:51:57 +0100

    filter students with result_level != level (errors)

--- sms/src/broad_sheet.py
+++ sms/src/broad_sheet.py
@@ -30,8 +30,11 @@ def get(acad_session, level=None, raw_score=False):
     :return:
     """
     # todo: * refactor generate_pdf_wrapper, use ProcessPoolExecutor for the whole generation: html, pdf, et al
-    #           handle spillovers (get passed 500l courses - asterisked - alongside the new ones),
-    #       * don't worry about length of the carryover column
+    #       * handle spillovers (get passed 500l courses - asterisked - alongside the new ones),
+    #           don't worry about length of the carryover column
+    #       * handle the last (2) column(s) for 500l broadsheet
+    #       * experiment with wkhtmltopdf's zoom and try to predict the zoom value to use with the
+    #           len_first_sem_carryovers, <...>, len(first_sem_course), <...> and len(options[0] + options[1])
 
     start = perf_counter()
     registered_students_for_session = get_filtered_student_by_level(acad_session, level)
@@ -45,7 +48,7 @@ def get(acad_session, level=None, raw_score=False):
     for level in sorted(registered_students_for_session.keys()):
         t1 = perf_counter()
         mat_nos = registered_students_for_session[level]
-        students, len_first_sem_carryovers, len_second_sem_carryovers = enrich_mat_no_list(mat_nos, acad_session)
+        students, len_first_sem_carryovers, len_second_sem_carryovers = enrich_mat_no_list(mat_nos, acad_session, level)
 
         level_courses = course_details.get_all(level)[0]
         first_sem_courses = [(x['course_code'], x['course_credit'], x['options']) for x in level_courses if x['course_semester'] == 1]
@@ -159,27 +162,33 @@ def get_level_at_acad_session(mat_no, acad_session):
     return ''
 
 
-def enrich_mat_no_list(mat_nos, acad_session):
+def enrich_mat_no_list(mat_nos, acad_session, level):
     students = {}
     max_len_first_sem_carryovers = 0
     max_len_second_sem_carryovers = 0
 
-    first = True
-
     for mat_no in mat_nos:
+
         course_reg = get_course_reg_at_acad_session(acad_session, mat_no=mat_no)
         if not course_reg:
             # print(mat_no, 'has no course_reg')
             continue
+
         result_details = get_results_for_acad_session(mat_no, acad_session)
         if result_details[1] != 200:
-            print(Fore.RED + mat_no, 'has no results')
+            print(Fore.RED + mat_no, 'has no results' + Style.RESET_ALL)
             continue
-        else:
-            result_details = result_details[0]
+        elif result_details[0]['level_written'] > 500 and level == 500:
+            # todo: fetch the passed 500 courses here and append ' *' to the grade and score
+            #       also fix color_map to support ' *'
+            pass
+        elif result_details[0]['level_written'] != level:
+            # print(mat_no, "result level", result_details[0]['level_written'], '!=', level)
+            continue
 
         personal_info = process_personal_info(mat_no)
-        result_details['tcr'] = course_reg['tcr']
+        result_details = result_details[0]
+        result_details['tcr'] = course_reg['tcr']  # this line must come before the sum_semester_credits() call
         sem_tcp, sem_tcr, sem_tcf, failed_courses = sum_semester_credits(result_details, grade_index=1, credit_index=2)
 
         result_details['semester_tcp'] = sem_tcp
@@ -188,7 +197,6 @@ def enrich_mat_no_list(mat_nos, acad_session):
         result_details['failed_courses'] = failed_courses
         result_details['othernames'] = personal_info['othernames']
         result_details['surname'] = personal_info['surname']
-        result_details['fullname'] = personal_info['othernames'] + ' ' + personal_info['surname']
 
         if len(result_details['carryovers']['first_sem']) > max_len_first_sem_carryovers:
             max_len_first_sem_carryovers = len(result_details['carryovers']['first_sem'])
@@ -216,8 +224,8 @@ def sum_semester_credits(result_details, grade_index, credit_index):
                 failed_courses.append(course)
             tcr[index] += courses[course][credit_index]
 
-    # if sum(tcp) != result_details['tcp'] or sum(tcr) != result_details['tcr']:
-    #     print('{}AssertionError: {} ==> tcp: {:>2} != {:>2}; tcr: {:>2} != {:>2}'.format(Fore.RED, Style.RESET_ALL
-    #           + result_details['mat_no'], sum(tcp), result_details['tcp'], sum(tcr), result_details['tcr']))
+    if sum(tcp) != result_details['tcp'] or sum(tcr) != result_details['tcr']:
+        print('{}AssertionError: {} ==> tcp: {:>2} != {:>2}; tcr: {:>2} != {:>2}'.format(Fore.RED, Style.RESET_ALL
+              + result_details['mat_no'], sum(tcp), result_details['tcp'], sum(tcr), result_details['tcr']))
 
     return tcp, tcr, tcf, failed_courses
