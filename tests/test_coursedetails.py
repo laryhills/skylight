7ccac2cbd080f994cf62780fde56017c3a333a13
lordfme <fortmax120@gmail.com>
2020-08-22 22:22:26 +0100

    updated course details tests

--- tests/test_coursedetails.py
+++ tests/test_coursedetails.py
@@ -10,35 +10,42 @@ level = 500
 valid_course = "MEE321"
 course_keys = ("course_code", "course_title", "course_credit", "course_semester", "course_level", "teaching_department", "start_date", "end_date", "options", "active")
 
-
-def get_course(course):
-    return cur.execute("SELECT * FROM courses WHERE course_code=?",(course,)).fetchone()
-
-
-def get_level(level):
-    return cur.execute("SELECT * FROM courses WHERE course_level=? AND active=?",(level, 1)).fetchall()
+sql_all_courses = "SELECT * FROM courses"
+sql_get_course = lambda course: ("SELECT * FROM courses WHERE course_code=?", (course,))
+sql_get_level = ("SELECT * FROM courses WHERE course_level=? AND active=?", (level, 1))
+sql_all_courses_active = ("SELECT * FROM courses WHERE active=?",(1,))
 
 
 def test_get_one_course():
     course_obj = course_details.get(valid_course)
-    course_row = get_course(valid_course)
+    course_row = cur.execute(*sql_get_course(valid_course)).fetchone()
     assert course_obj["teaching_dept"] == course_row["teaching_department"]
     for prop in set(course_keys) - {"teaching_department"}:
         assert course_obj[prop] == course_row[prop]
 
 def test_get_all_level():
-    lvl_courses = course_details.get_all(level, options=True, inactive=False)
-    lvl_courses_row = get_level(level)
+    lvl_courses = course_details.get_all(level, True, False)
+    lvl_courses_row = cur.execute(*sql_get_level).fetchall()
     lvl_codes = set([x["course_code"] for x in lvl_courses_row])
     for course in lvl_courses:
        lvl_codes.remove(course["course_code"])
     assert len(lvl_codes) == 0
 
 
-def test_get_all_level_options():
-    all_options = set([x["course_code"] for x in course_details.get_all(level, options=True, inactive=False)])
-    one_option = set([x["course_code"] for x in course_details.get_all(level, options=False, inactive=False)])
+def test_get_all_options():
+    all_options = set([x["course_code"] for x in course_details.get_all(level, True, False)])
+    one_option = set([x["course_code"] for x in course_details.get_all(level, False, False)])
     diff = len(all_options - one_option)
-    diff_row = [x for x in get_level(level) if x["options"] != 0]
+    diff_row = [x for x in cur.execute(*sql_get_level).fetchall() if x["options"] != 0]
     options = len(set([x["options"] for x in diff_row]))
     assert diff == len(diff_row) - options
+
+
+def test_get_all_default():
+    all_courses = course_details.get_all(None, True, False)
+    sql_all_courses = cur.execute(*sql_all_courses_active).fetchall()
+    assert len(sql_all_courses) == len(all_courses)
+
+
+def test_get_all_inc_inactive():
+    pass 
