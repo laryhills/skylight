76d588105480f44efb8a5e8b9965c56c0327ac83
lordfme <fortmax120@gmail.com>
2020-08-25 08:40:41 +0100

    added put and del tests course_details

--- tests/test_coursedetails.py
+++ tests/test_coursedetails.py
@@ -10,17 +10,19 @@ level = 500
 valid_course = "MEE321"
 course_keys = ("course_code", "course_title", "course_credit", "course_semester", "course_level", "teaching_department", "start_date", "end_date", "options", "active")
 inv_crs_val = ("INV211", "Invalid Course", 2, 1, 200, "MEE", 1970, 2999, 0, 0)
+inv_crs_val_2 = ("INV311", "Invalid Course-2", 2, 1, 300, "MEE", 1970, 2999, 0, 0)
 
 sql_all_courses = "SELECT * FROM courses"
 sql_get_course = lambda course=valid_course: ("SELECT * FROM courses WHERE course_code=?", (course,))
 sql_get_level = lambda level=level: ("SELECT * FROM courses WHERE course_level=? AND active=?", (level, 1))
 sql_all_courses_active = ("SELECT * FROM courses WHERE active=?",(1,))
-sql_ins_inactive = ("INSERT INTO courses VALUES (?,?,?,?,?,?,?,?,?,?)", inv_crs_val)
+sql_ins_course = lambda course=inv_crs_val: ("INSERT INTO courses VALUES (?,?,?,?,?,?,?,?,?,?)", (course,))
 sql_del_course = lambda course=inv_crs_val[0]: ("DELETE FROM courses WHERE course_code=?",(course,))
 
 
 def test_setup_env():
     cur.execute(*sql_del_course())
+    cur.execute(*sql_del_course(inv_crs_val_2[0]))
     conn.commit()
 
 
@@ -57,7 +59,7 @@ def test_get_all_default():
 
 
 def test_get_all_inactive():
-    cur.execute(*sql_ins_inactive)
+    cur.execute(*sql_ins_course())
     conn.commit()
     curr_actives = set([x["course_code"] for x in course_details.get_all(None, True, False)])
     curr_all = set([x["course_code"] for x in course_details.get_all(None, True, True)])
@@ -80,6 +82,59 @@ def test_post():
     assert course_details.post(course_add) == (None, 200)
     assert course_details.post(course_add)[1] == 400
     cur.execute(*sql_del_course())
+    conn.commit()
+
+
+def test_delete():
+    assert course_details.delete(inv_crs_val[0]) == (None, 404)
+    cur.execute(*sql_ins_course())
+    conn.commit()
+    assert course_details.delete(inv_crs_val[0]) == (None, 200)
+    course_row = cur.execute(*sql_get_course(inv_crs_val[0])).fetchone()
+    assert course_row == None
+
+
+def test_put_one():
+    course_add = dict(zip(course_keys, inv_crs_val))
+    course_add["course_credit"] = 3
+    course_add["course_semester"] = 2
+    course_list = [course_add]
+    assert course_details.put(course_add) == (None, 404)
+    cur.execute(*sql_ins_course())
+    conn.commit()
+    assert course_details.put(course_list) == (None, 200)
+    course_row = cur.execute(*sql_get_course(inv_crs_val[0])).fetchone()
+    assert course_row["course_credit"] == 3
+    assert course_row["course_semester"] == 2
+    cur.execute(*sql_del_course())
+    conn.commit()
+
+
+def test_put_mult():
+    course_add = dict(zip(course_keys, inv_crs_val))
+    course_add["course_credit"] = 3
+    course_add["course_semester"] = 2
+    course_add_2 = dict(zip(course_keys, inv_crs_val_2))
+    course_add_2["start_date"] = 2012
+    course_add_2["options"] = 3
+    course_list = [course_add, course_add_2]
+    cur.execute(*sql_ins_course())
+    conn.commit()
+    assert course_details.put(course_list) == (None, 404)
+    course_row = cur.execute(*sql_get_course(inv_crs_val[0])).fetchone()
+    assert course_row["course_credit"] != 3
+    assert course_row["course_semester"] != 2
+    cur.execute(*sql_ins_course(inv_crs_val_2))
+    conn.commit()
+    assert course_details.put(course_list) == (None, 200)
+    course_row = cur.execute(*sql_get_course(inv_crs_val[0])).fetchone()
+    assert course_row["course_credit"] == 3
+    assert course_row["course_semester"] == 2
+    course_row = cur.execute(*sql_get_course(inv_crs_val_2[0])).fetchone()
+    assert course_row["start_date"] == 2012
+    assert course_row["options"] == 3
+    cur.execute(*sql_del_course())
+    cur.execute(*sql_del_course(inv_crs_val_2[0]))
     conn.commit()
 
 
