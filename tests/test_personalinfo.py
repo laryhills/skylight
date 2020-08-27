a1567ba6fae378c480a65bb181e7965ea3dab1d7
lordfme <fortmax120@gmail.com>
2020-08-27 15:50:45 +0100

    removed grad_stats from SQL rows in tests, account for new handling method

--- tests/test_personalinfo.py
+++ tests/test_personalinfo.py
@@ -7,7 +7,9 @@ from random import sample
 from sms.src import personal_info
 from tests import db_path
 
-conn = sqlite3.connect(os.path.join(db_path, "2015-2016.db"))
+sessional_db = "2015-2016.db"
+
+conn = sqlite3.connect(os.path.join(db_path, sessional_db))
 conn.row_factory = sqlite3.Row
 cur = conn.cursor()
 
@@ -17,9 +19,9 @@ cur_2 = conn_2.cursor()
 
 perms = {"read": True, "write": True, "superuser": True, "levels": [100, 200, 300, 600], "usernames": ["personalinfo_test"]}
 
-info_keys = ['mat_no', 'surname', 'othernames', 'mode_of_entry', 'session_admitted', 'session_grad', 'level', None, 'sex', 'date_of_birth', 'state_of_origin', 'lga', 'phone_no', 'email_address', 'sponsor_phone_no', 'sponsor_email_address', 'grad_stats', None, None, None]
-row_keys = ["MATNO", "SURNAME", "OTHERNAMES", "MODE_OF_ENTRY", "SESSION_ADMIT", "SESSION_GRADUATED", "CURRENT_LEVEL", "OPTION", "SEX", "DATE_OF_BIRTH", "STATE_OF_ORIGIN", "LGA_OF_ORIGIN", "PHONE_NO", "EMAIL_ADDRESS", "SPONSOR_PHONE_NO", "SPONSOR_EMAIL_ADDRESS", "GRAD_STATUS", "PROBATED_TRANSFERRED", "IS_SYMLINK", "DATABASE"]
-row_values = ["ENGTESTING", "Banks", "Ian", 1, 2015, None, 100, None, "M", "8/12/12", "Edo", "Oredo", "08033104028", "email@gmail.com", "08033104028", "dad@dadmail.com", None, 0, 0, None]
+info_keys = ['mat_no', 'surname', 'othernames', 'mode_of_entry', 'session_admitted', 'session_grad', 'level', None, 'sex', 'date_of_birth', 'state_of_origin', 'lga', 'phone_no', 'email_address', 'sponsor_phone_no', 'sponsor_email_address', None, None, None]
+row_keys = ["MATNO", "SURNAME", "OTHERNAMES", "MODE_OF_ENTRY", "SESSION_ADMIT", "SESSION_GRADUATED", "CURRENT_LEVEL", "OPTION", "SEX", "DATE_OF_BIRTH", "STATE_OF_ORIGIN", "LGA_OF_ORIGIN", "PHONE_NO", "EMAIL_ADDRESS", "SPONSOR_PHONE_NO", "SPONSOR_EMAIL_ADDRESS", "PROBATED_TRANSFERRED", "IS_SYMLINK", "DATABASE"]
+row_values = ["ENGTESTING", "Banks", "Ian", 1, 2015, None, 100, None, "M", "8/12/12", "Edo", "Oredo", "08033104028", "email@gmail.com", "08033104028", "dad@dadmail.com", 0, 0, None]
 info_base = dict(zip(info_keys, row_values))
 info_base.pop(None)
 
@@ -29,8 +31,8 @@ def get_student(mat_no):
 
 
 def insert_student(dummy_info):
-    cur.execute("INSERT INTO PersonalInfo VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", dummy_info)
-    cur_2.execute("INSERT INTO Main VALUES(?,?)", (dummy_info[0], "2015-2016.db"))
+    cur.execute("INSERT INTO PersonalInfo VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", dummy_info)
+    cur_2.execute("INSERT INTO Main VALUES(?,?)", (dummy_info[0], sessional_db))
     conn.commit()
     conn_2.commit()
 
@@ -60,6 +62,7 @@ def test_get_valid_info():
     info_rows = cur.execute("SELECT * FROM PersonalInfo").fetchall()
     info_row = sample(info_rows, 1)[0]
     student_data = personal_info.get(info_row["matno"])
+    student_data["level"] *= [1,-1][student_data["grad_status"]]
     for prop_data, prop_row in zip(info_keys, row_keys):
         if prop_data:
             assert student_data[prop_data] == info_row[prop_row]
@@ -72,6 +75,7 @@ def test_get_valid_dets():
     output, ret_code = personal_info.get_exp(info_row["matno"])
     assert ret_code == 200
     student_data = loads(output)
+    student_data["level"] *= [1,-1][student_data["grad_status"]]
     for prop_data, prop_row in zip(info_keys, row_keys):
         if prop_data:
             assert student_data[prop_data] == info_row[prop_row]
@@ -80,6 +84,7 @@ def test_get_valid_dets():
 def test_post_dets_info():
     config.add_token("TESTING_token", "personalinfo_test", perms)
     dummy_info = info_base.copy()
+    dummy_info["grad_status"] = int(0 > dummy_info["level"])
     output, ret_code = personal_info.post_exp(dummy_info)
     assert (output, ret_code) == (None, 200)
     info_row = get_student(dummy_info["mat_no"])
@@ -89,6 +94,7 @@ def test_post_dets_info():
     delete_student(dummy_info["mat_no"])
 
 
+# TODO fix from below
 def test_post_dets_update_errors():
     config.add_token("TESTING_token", "personalinfo_test", perms)
     insert_student(row_values)
