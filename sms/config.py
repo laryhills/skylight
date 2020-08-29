0a823ea780656750cc09714b2037439e9dd93f35
chima <chimaataman@gmail.com>
2020-08-29 08:49:54 +0100

    add backup_dir to config.py

--- sms/config.py
+++ sms/config.py
@@ -6,21 +6,26 @@ from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 
 base_dir = os.path.dirname(__file__)
-db_path = os.path.join(base_dir, 'database')
+cache_base_dir = os.path.join(os.path.expanduser('~'), 'sms', 'cache_mechanical')
+backups_dir = os.path.join(os.path.expanduser('~'), 'sms', 'backups_mechanical')
+DB_DIR = os.path.join(base_dir, 'database')
+
+[os.makedirs(path) for path in (cache_base_dir, backups_dir) if not os.path.exists(path)]
+
 
 start_session = 2003
 end_session = 2019  # TODO query current_session from master DB, (don't use utils)
 
-sqlalchemy_binds = {'master': 'sqlite:///' + os.path.join(db_path, 'master.db'),
-                    'courses': 'sqlite:///' + os.path.join(db_path, 'courses.db'),
-                    'logs': 'sqlite:///' + os.path.join(db_path, 'logs.db')}
-sqlalchemy_binds.update({'{}-{}'.format(num, num + 1): 'sqlite:///' + os.path.join(db_path, '{}-{}.db'.format(num, num + 1)) for num in range(start_session, end_session + 1)})
+sqlalchemy_binds = {'master': 'sqlite:///' + os.path.join(DB_DIR, 'master.db'),
+                    'courses': 'sqlite:///' + os.path.join(DB_DIR, 'courses.db'),
+                    'logs': 'sqlite:///' + os.path.join(DB_DIR, 'logs.db')}
+sqlalchemy_binds.update({'{}-{}'.format(num, num + 1): 'sqlite:///' + os.path.join(DB_DIR, '{}-{}.db'.format(num, num + 1)) for num in range(start_session, end_session + 1)})
 
 connex_app = connexion.App(__name__, specification_dir=base_dir)
 app = connex_app.app
 
 app.config['SECRET_KEY'] = secrets.token_hex(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(db_path, 'accounts.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(DB_DIR, 'accounts.db')
 app.config['SQLALCHEMY_BINDS'] = sqlalchemy_binds
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
