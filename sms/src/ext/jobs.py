a121ce709fe48cbb2817b77889316ee65f3ec8a8
chima <chimaataman@gmail.com>
2020-08-30 14:33:34 +0100

    restructure jobs.py

--- sms/src/ext/jobs.py
+++ sms/src/ext/jobs.py
@@ -14,40 +14,6 @@ from sms.src.users import log
 from sms.config import DB_DIR, BACKUP_DIR, CACHE_BASE_DIR
 
 
-def clear_cache_dir():
-    for file in os.scandir(CACHE_BASE_DIR):
-        file_path = file.path
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
-    log(user='system', func=clear_cache_dir, qual_name='jobs.clear_cache_dir', args=[], kwargs={})
-
-
-def backup_databases(before_restore=False):
-    datetime_tag = datetime.now().isoformat().split('.')[0].replace('T', '__')
-    flag = '__before_restore' if before_restore else ''
-    zip_file = os.path.join(BACKUP_DIR, 'databases__' + datetime_tag + flag + '.zip.skylight')
-    with ZipFile(zip_file, 'w', ZIP_DEFLATED) as zf:
-        # remove sorted and use os.scandir instead of listdir if performance becomes an issue
-        databases = sorted([file_name for file_name in os.listdir(DB_DIR) if file_name.endswith('.db')])
-        for file_name in databases:
-            zf.write(os.path.join(DB_DIR, file_name), arcname=file_name)
-    log(user='system', func=backup_databases, qual_name='jobs.backup_databases', args=[], kwargs={})
-
-
-def my_listener(event):
-    if event.code == EVENT_JOB_EXECUTED:
-        pass
-    elif event.exception or event.code == EVENT_JOB_ERROR:
-        print('The job {} crashed :('.format(event.job_id))
-    elif event.code == EVENT_JOB_MISSED:
-        print('Missed job {}'.format(event.job_id))
-
-
 def start_scheduled_jobs():
     timezone = tz('Africa/Lagos')
     backup_trigger = CronTrigger(hour=17, minute=5, jitter=120)
@@ -60,10 +26,11 @@ def start_scheduled_jobs():
     }
 
     _scheduler = BackgroundScheduler(timezone=timezone, job_defaults=job_defaults)
-    _scheduler.add_listener(my_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR | EVENT_JOB_MISSED)
+    _scheduler.add_listener(job_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR | EVENT_JOB_MISSED)
 
-    job_1 = _scheduler.add_job(backup_databases, id='backup_databases', trigger=backup_trigger)
-    job_2 = _scheduler.add_job(clear_cache_dir, id='clear_cache_dir', trigger=cache_trigger)
+    jobs = []
+    jobs.append(_scheduler.add_job(backup_databases, id='backup_databases', trigger=backup_trigger))
+    jobs.append(_scheduler.add_job(clear_cache_dir, id='clear_cache_dir', trigger=cache_trigger))
 
     try:
         _scheduler.start()
@@ -71,4 +38,47 @@ def start_scheduled_jobs():
         print('f')
         pass
 
-    return _scheduler, [job_1, job_2]
+    return _scheduler, jobs
+
+
+def job_listener(event):
+    if event.code == EVENT_JOB_EXECUTED:
+        pass
+    elif event.exception or event.code == EVENT_JOB_ERROR:
+        print('The job {} crashed :('.format(event.job_id))
+    elif event.code == EVENT_JOB_MISSED:
+        print('Missed job {}'.format(event.job_id))
+
+
+# ==============================================================================
+# ================================  JOBS  ======================================
+# ==============================================================================
+
+def backup_databases(before_restore=False, external=False):
+    datetime_tag = datetime.now().isoformat().split('.')[0].replace('T', '__')
+    flag = '__before_restore' if before_restore else ''
+    backup_name = 'databases__' + datetime_tag + flag + '.zip.skylight'
+
+    databases = sorted([file.name for file in os.scandir(DB_DIR) if file.name.endswith('.db')])
+    zip_file = os.path.join(BACKUP_DIR, backup_name)
+    with ZipFile(zip_file, 'w', ZIP_DEFLATED) as zf:
+        for file_name in databases:
+            zf.write(os.path.join(DB_DIR, file_name), arcname=file_name)
+
+    if not external:
+        log(user='system', func=backup_databases, qual_name='jobs.backup_databases', args=[], kwargs={})
+
+    return backup_name
+
+
+def clear_cache_dir():
+    for file in os.scandir(CACHE_BASE_DIR):
+        file_path = file.path
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    log(user='system', func=clear_cache_dir, qual_name='jobs.clear_cache_dir', args=[], kwargs={})
