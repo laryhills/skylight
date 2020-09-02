6ce26feb8339d077608e7188e3188cb11c338886
chima <chimaataman@gmail.com>
2020-09-02 05:12:27 +0100

    change backups.delete param to str

--- sms/src/backups.py
+++ sms/src/backups.py
@@ -57,7 +57,7 @@ def fetch_backup_list():
 def download_backups(backup_names=None, limit=15):
     if not backup_names:
         backup_names = sorted([backup.name for backup in os.scandir(BACKUP_DIR)], reverse=True)[:limit]
-    file_name = token_hex(8) + '.zip'
+    file_name = 'backups_' + token_hex(8) + '.zip'
     try:
         with ZipFile(os.path.join(CACHE_BASE_DIR, file_name), 'w', ZIP_DEFLATED) as zf:
             for backup_name in backup_names:
@@ -98,18 +98,19 @@ def restore_backup(backup_name, include_accounts=False):
                     continue
                 zf.extract(database, DB_DIR)
     except FileNotFoundError:
-        return 'Database not found on server', 404
+        return 'File "{}" not found on server'.format(backup_name), 404
     except Exception as e:
         return 'Something went wrong', 400
     return None, 200
 
 
-def delete_backup(backup_names):
-    for backup_name in backup_names:
-        backup_path = os.path.join(BACKUP_DIR, backup_name)
-        if os.path.isfile(backup_path):
-            try:
-                os.unlink(backup_path)
-            except OSError:
-                pass
+def delete_backup(backup_name):
+    backup_path = os.path.join(BACKUP_DIR, backup_name)
+    if os.path.isfile(backup_path):
+        try:
+            os.unlink(backup_path)
+        except OSError:
+            pass
+    else:
+        return 'File "{}" not found on server'.format(backup_name), 404
     return None, 200
