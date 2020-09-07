80c516bca213f0fa9c8d4e6a43dddaafd76a08ed
lordfme <fortmax120@gmail.com>
2020-09-07 12:29:17 +0100

    added delete and delete_all to logs

--- sms/src/logs.py
+++ sms/src/logs.py
@@ -1,6 +1,8 @@
 from sms.src.users import fn_props
 from sms.models.logs import Logs
 from json import loads
+from sms.config import db
+from sms.src.users import access_decorator
 
 
 # todo: implement access controls w/o logging
@@ -16,3 +18,24 @@ def get(limit=5, offset=0):
             offset = 0
     log_list = Logs.query.offset(offset).limit(limit).all()
     return [(log.timestamp, fn_props[log.operation]["logs"](log.user, loads(log.params))) for log in log_list]
+
+
+@access_decorator
+def delete(ids):
+    logs = []
+    for _id in ids:
+        logs.append(Logs.query.filter_by(id=_id).first())
+    if all(logs):
+        for log in logs:
+            db.session.delete(log)
+        db.session.commit()
+    else:
+        return None, 404
+    return None, 200
+
+
+@access_decorator
+def delete_all():
+    Logs.query.delete()
+    db.session.commit()
+    return None, 200
