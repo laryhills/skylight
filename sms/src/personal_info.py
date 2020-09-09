9ba4dd81a9655553e09df8df354ebfed2e32ebf0
lordfme <fortmax120@gmail.com>
2020-09-09 12:51:48 +0100

    allow for handling DE students in personal_info post

--- sms/src/personal_info.py
+++ sms/src/personal_info.py
@@ -1,8 +1,8 @@
-from json import dumps
+from json import dumps, loads
 from sms.config import db
 from sms.src import utils
 from sms.src.users import access_decorator
-from sms.models.master import MasterSchema
+from sms.models.master import MasterSchema, Props
 
 all_fields = {'date_of_birth', 'email_address', 'grad_status', 'level', 'lga', 'mat_no', 'mode_of_entry', 'othernames',
               'phone_no', 'session_admitted', 'session_grad', 'sex', 'sponsor_email_address', 'sponsor_phone_no',
@@ -52,6 +52,9 @@ def post(data):
     if not all([data.get(prop) for prop in required]) or (data.keys() - all_fields):
         # Empty value supplied or Invalid field supplied or Missing field present
         return "Invalid field supplied or missing a compulsory field"
+    if ("grad_status" not in data) or not data.get("session_grad", True):
+        # Check exceptions to non required
+        return "Invalid field supplied or missing a compulsory field"
 
     session_admitted = data['session_admitted']
 
@@ -61,7 +64,7 @@ def post(data):
 
     session = utils.load_session(session_admitted)
     personalinfo_schema = session.PersonalInfoSchema()
-    data["is_symlink"] = 0
+    data["is_symlink"] = data["mode_of_entry"] - 1
     data["level"] = abs(data["level"]) * [1,-1][data.pop("grad_status")]
     student_model = personalinfo_schema.load(data)
 
@@ -69,6 +72,20 @@ def post(data):
     db.session.commit()
 
     db_session = personalinfo_schema.Meta.sqla_session
+
+    if data["mode_of_entry"] != 1:
+        # Point from Symlink table
+        session_list = loads(Props.query.filter_by(key="SessionList").first().valuestr)
+        class_session = session_list[session_list.index(session_admitted) - data["mode_of_entry"] + 1]
+        student_model.database = "{}-{}.db".format(class_session, class_session + 1)
+        symlink_session = utils.load_session(class_session)
+        symlink_schema = symlink_session.SymLinkSchema()
+        database = "{}-{}.db".format(session_admitted, session_admitted + 1)
+        symlink_model = symlink_schema.load({"mat_no": data["mat_no"], "database": database})
+        db_session_2 = symlink_schema.Meta.sqla_session
+        db_session_2.add(symlink_model)
+        db_session_2.commit()
+
     db_session.add(student_model)
     db_session.commit()
 
