98673ccf215f0d9b957e478660846f750821a25a
ucheigbeka <44610243+ucheigbeka@users.noreply.github.com>
2020-09-15 17:13:33 +0100

    Added the Props table and the surname column to the master db.

--- setup/src/personal_info.py
+++ setup/src/personal_info.py
@@ -16,13 +16,17 @@ path = os.path.join(setup_data_dir, 'Personal_Data.csv')
 frame = pd.read_csv(path, dtype={'PHONE_NO': np.str})
 
 start_session = 2003
-#start_session = 2017
 curr_session = 2019
 
 
 def format_options(data):
     if isinstance(data, float): return np.nan
     return '{},{}2'.format(data, data[:-1])
+
+
+def create_props():
+    with open('props.sql') as fd:
+        master.executescript(fd.read())
 
 
 def create_table_schema():
@@ -36,7 +40,8 @@ def create_table_schema():
                   'SEX TEXT, DATE_OF_BIRTH REAL, STATE_OF_ORIGIN TEXT, LGA_OF_ORIGIN TEXT, PHONE_NO TEXT, ' \
                   'EMAIL_ADDRESS TEXT, SPONSOR_PHONE_NO TEXT, SPONSOR_EMAIL_ADDRESS TEXT, PROBATED_TRANSFERRED ' \
                   'INTEGER, IS_SYMLINK INTEGER, DATABASE TEXT);'
-    sym_link_stmt = '''CREATE TABLE SymLink(MATNO TEXT PRIMARY KEY, DATABASE TEXT); '''
+    sym_link_stmt = 'CREATE TABLE SymLink(MATNO TEXT PRIMARY KEY' + \
+                    ''.join(map(lambda x: ', DATABASE_{} TEXT'.format(x), range(1, 9))) + ');'
     for session in range(start_session, curr_session + 1):
         curr_db = '{}-{}.db'.format(session, session + 1)
         conn = sqlite3.connect(os.path.join(db_base_dir, curr_db))
@@ -60,27 +65,7 @@ def populate_db(conn, session):
     curr_frame.to_sql('PersonalInfo', conn, index=False, if_exists='append')
     conn.commit()
 
-    # # Symbolic link
-    #     # Regular students
-    # sym_links_frame = curr_frame[curr_frame.IS_SYMLINK == 1]
-    # sym_links_frame = sym_links_frame[sym_links_frame.MODE_OF_ENTRY == 1]
-    # sym_links_group = sym_links_frame.groupby(by='DATABASE')
-    # groups = [sym_links_group.get_group(x) for x in sym_links_group.groups]
-    # for group in groups:
-    #     new_db = group['DATABASE'].iloc[0]
-    #     new_conn = sqlite3.connect(os.path.join(db_base_dir, new_db))
-    #     group['DATABASE'] = curr_db
-    #     df = group[['MATNO', 'DATABASE']].drop_duplicates(subset='MATNO')
-    #     df.to_sql('SymLink', new_conn, index=False, if_exists='append')
-    # 
-    #     # Direct entry students
-    # de_frame = frame[(frame.SESSION_ADMIT == session + 1) & (frame.MODE_OF_ENTRY == 2)]
-    # de_frame['DATABASE'] = '{0}-{1}.db'.format(session + 1, session + 2)
-    # #de_frame.DATABASE.where(de_frame.CURRENT_LEVEL == ((curr_session + 1 - session) * 100), '{0}-{1}.db'.format(curr_session - (de_frame.CURRENT_LEVEL / 100), curr_session - (int(curr_frame.CURRENT_LEVEL) / 100) + 1), inplace=True)
-    # sym_de_frame = de_frame[['MATNO', 'DATABASE']].drop_duplicates(subset='MATNO')
-    # sym_de_frame.to_sql('SymLink', conn, index=False, if_exists='append')
-
-    master_frame = curr_frame.MATNO.to_frame()
+    master_frame = curr_frame[['MATNO', 'SURNAME']].copy()
     master_frame['DATABASE'] = curr_db
     master_frame.to_sql('Main', master, index=False, if_exists='append')
     master.commit()
@@ -88,7 +73,7 @@ def populate_db(conn, session):
 
 # master database
 master = sqlite3.connect(os.path.join(db_base_dir, 'master.db'))
-try: master.execute('CREATE TABLE Main (MATNO TEXT PRIMARY KEY, DATABASE TEXT)')
+try: master.execute('CREATE TABLE Main (MATNO TEXT PRIMARY KEY, SURNAME TEXT, DATABASE TEXT)')
 except sqlite3.OperationalError: pass
 
 create_table_schema()
@@ -101,5 +86,6 @@ for session in sessions:
     populate_db(conn, session)
     conn.close()
 
+create_props()
 master.close()
 print('done')
